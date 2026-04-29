d7adbd3ba3916c907cc6f96d024d04cf2ad25a35
maxbarkouras <max@computer>
2026-04-29 14:35:04 -0700

    assembler updates

--- assembler.py
+++ assembler.py
@@ -1,3 +1,6 @@
+# assembler outputs machine code in format designed to be pasted into instruction memory file
+import sys
+
 opcodes = {
     "add"  : "000",
     "sub"  : "001",
@@ -14,16 +17,29 @@ registers = {
     "r3" : "11"
 }
 
-with open("max.asm", "r") as file:
-    for line in file.readlines():
-        line = line.strip()
-        op, reg, val = line.split(" ")
-        reg = reg.split(",")[0]
+def main():
 
-        opval = opcodes[op]
-        if opval[0] == "1":
-            print(f"0b{opval}{registers[reg]}{int(val):02b}")
+    if(len(sys.argv) < 2):
+        print("No input file argument passed! Please rerun with assembler.py {asm_file}")
+        exit()
 
-        else:
-            print(f"0b{opval}{registers[reg]}{registers[val]}")
+    with open(sys.argv[1], "r") as file:
+        count=0
+        for line in file.readlines():
+            line = line.strip()
+            if line.startswith(";") or line == "": continue
 
+            op, reg, val = line.split(" ")
+            reg = reg.split(",")[0]
+
+            opval = opcodes[op]
+            if opval[0] == "1":
+                print(f"mem[{count}] = 7'b{opval}{registers[reg]}{int(val):02b};")
+            else:
+                print(f"mem[{count}] = 7'b{opval}{registers[reg]}{registers[val]};")
+
+            count+=1
+
+
+if __name__ == "__main__":
+    main()
